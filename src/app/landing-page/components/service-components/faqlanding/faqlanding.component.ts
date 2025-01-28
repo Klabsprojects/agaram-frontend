229f66965b8af57d2b0cc3262fc7769adbac26a0
Manimaranboss <manimaran@klabsindia.com>
2025-01-28 18:50:00 +0530

    changes : faq,utility

--- src/app/landing-page/components/service-components/faqlanding/faqlanding.component.ts
+++ src/app/landing-page/components/service-components/faqlanding/faqlanding.component.ts
@@ -37,8 +37,10 @@ export class FaqComponentlanding {
           value && value.toString().toLowerCase().includes(filterText)));
     }
   }
+  public startIndex:any;
   pagedData() {
     const startIndex = (this.currentPage - 1) * this.pageSize;
+    this.startIndex = startIndex;
     const endIndex = startIndex + this.pageSize;
     return this.filteredEmployeeList.slice(startIndex, endIndex);
   }
