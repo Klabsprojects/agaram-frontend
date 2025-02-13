e18d063bb36844aea699e6ff703a2fa1755aca9c
Manimaranboss <manimaran@klabsindia.com>
2025-02-13 17:48:42 +0530

    changes : Footer visitor changes

--- src/app/landing-page/footer/footer.component.ts
+++ src/app/landing-page/footer/footer.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
-
+import { LeaveTransferService } from '../../forms/forms.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css','../landing-page.component.css']
+  styleUrls: ['./footer.component.css', '../landing-page.component.css']
 })
 export class FooterComponent {
   activeSubmenu: string | null = null;
   submenuTimeout: any;
   menu = false;
-  serviceEnable:any
+  serviceEnable: any
+  visitors: any = 0;
+  year: number = new Date().getFullYear();
+  updatedYear:any;
+  constructor(private leaveTransferService: LeaveTransferService) { }
   ngOnInit(): void {
     this.serviceEnable = localStorage.getItem('loginAs');
+    this.getVisitors();
   }
-  toggle(){
+  toggle() {
     this.menu = !this.menu;
   }
   toggleSubmenu(submenu: string) {
@@ -38,5 +43,20 @@ export class FooterComponent {
     if (this.submenuTimeout) {
       clearTimeout(this.submenuTimeout);
     }
+  }
+  getVisitors(){
+    this.leaveTransferService.uploadGet('getLatestVisitor').subscribe((res:any)=>{
+      this.visitors = res.results.count;
+      this.updatedYear = res.results.visitUpdateDate.split("T")[0]
+      this.increaseVisitor();
+    })
+  }
+  increaseVisitor() {
+    var body = {
+      "count": this.visitors += 1
+    }
+    this.leaveTransferService.uploadAdd(body, 'addOrUpdateVisitor').subscribe((res: any) => {
+      this.visitors = res.results.count;
+    })
   }
 }
